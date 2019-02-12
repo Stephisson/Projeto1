fad400e50afee72becaca29f6e38c2f8bd00e13e
“Stephisson <36118052017.2N@senacrjedu.local>
2019-02-12 21:25:07 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/senac/PrimeiroProjetoApplication.java
@@ -0,0 +1,14 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+
+}
+
